b76910e2739e12415751fd3c9bc595fbda895b4e
LiangGuanyu <lianggy0719@126.com>
2015-05-22 10:27:30 +0800

    convert into a node package

---
+++ index.js
@@ -0,0 +1,12 @@
+exports.Cache = function() {
+  return require('./lib/cache').Cache;
+}
+
+exports.Flowctl = function() {
+  return require('./lib/flowctl');
+}
+
+exports.Json4line = function() {
+  return require('./lib/json4line');
+}
+
